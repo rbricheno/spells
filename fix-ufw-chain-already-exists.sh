03fd1432afc27ac2a3ba1cca35c75f6cdbd9c2d9
Robert Bricheno <github2@ambrosia.org.uk>
2020-02-19 21:51:27 +0000

    More information about ufw "chain already exists"

--- fix-ufw-chain-already-exists.sh
+++ fix-ufw-chain-already-exists.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 # Spell to fix errors like:
 # ERROR: initcaps [Errno 2] ip6tables: Chain already exists.
-# (Which you might get when frobbing UFW with ansible.)
+# (Which you might get when frobbing ufw with ansible on older Ubuntu systems.)
+# Seems to be caused by a ufw bug, fix is in Ubuntu 18.04
+# More info here https://bugs.launchpad.net/ufw/+bug/1204579
 ufw disable
 sleep 1
 iptables -F
