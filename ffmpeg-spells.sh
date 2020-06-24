b8535108e7d5bb826b44c3159afdaa14461ae533
Robert Bricheno <github2@ambrosia.org.uk>
2020-06-24 11:32:48 +0100

    Update ffmpeg-spells.sh

--- ffmpeg-spells.sh
+++ ffmpeg-spells.sh
@@ -1,2 +1,5 @@
 # Increase the volume of a file to 200% of the original
 ffmpeg -i input.mkv -vcodec copy -filter:a "volume=2.000000" output.louder.mkv
+
+# Convert a file to a format that iMovie can import
+ffmpeg -i input.avi -pix_fmt yuv420p output.mp4
